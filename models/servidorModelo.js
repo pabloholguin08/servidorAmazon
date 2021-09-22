232171e86e2caccc05a5b5aaadfff6e0a6277a2f
pabloholguin08 <jpholguin06@gmail.com>
2021-09-22 16:53:39 -0500

    update 22/09/2021

--- models/servidorModelo.js
+++ models/servidorModelo.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const {conectarBD}=require('../database/conexion.js')
+const {conectarBD}=require('../database/conexion.js');
+const FacturaModelo= require('../models/facturaModelo.js');
 
 class ServidorModelo{
     constructor(){
         this.app = express();
         this.levantarBD();
+        this.auxiliares();
         this.llamarRutas();
     }
 
@@ -18,13 +20,31 @@ class ServidorModelo{
         conectarBD();
     }
 
+    auxiliares(){
+        this.app.use(express.json())
+    }
+
     llamarRutas(){
         this.app.get('/facturas/consultar', function (req, res) {
             res.send('Buenas Tardes Juan Pablo');
         });
 
-        this.app.post('/facturas/nuevo', function (req, res) {
-            res.send('Buenas Tardes Juan Pablo');
+        this.app.post('/facturas/nuevo', async function (req, res) {
+            let datosFactura=req.body;
+            try{
+                let factura= new FacturaModelo(datosFactura);
+                await factura.save();
+                res.status(200).json({
+                    respuesta:"exito",
+                    datos:factura
+                });
+                
+            }catch(error){
+                res.status(400).jason({
+                    respuesta:"error",
+                    datos:error
+                })
+            }
         });
            
         this.app.put('/facturas/cambiar', function (req, res) {
